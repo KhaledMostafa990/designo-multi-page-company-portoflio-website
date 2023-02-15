9a68892cdf54bb1a5d82c577765d7656b914dc4b
DEEP <khaledfarghly9991@gmail.com>
2023-02-15 23:02:54 +0200

    getting started with strapi-cms and create basic data models

---
+++ designo-strapi-cms/src/api/designo/controllers/designo.ts
@@ -0,0 +1,7 @@
+/**
+ * designo controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::designo.designo');
---
+++ designo-strapi-cms/src/api/designo/routes/designo.ts
@@ -0,0 +1,7 @@
+/**
+ * designo router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::designo.designo');
