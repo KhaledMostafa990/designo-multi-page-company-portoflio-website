5cb7220f08fec03969bf222bbe303650b6e16911
Khaled Farghaly <khaledfarghly9991@gmail.com>
2025-09-29 15:39:45 +0300

    test: add jest config and initial useScrollDirection tests; ci: add GitHub Actions workflow

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({ dir: './' });
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+export default createJestConfig(config);
